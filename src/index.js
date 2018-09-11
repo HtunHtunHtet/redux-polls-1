5eb4de14bdfedb0ad483865b9012e8bedae239cb
htunhtunhtet <ryanhhh91@gmail.com>
2018-09-11 15:39:14 +0800

    reducers

--- src/index.js
+++ src/index.js
@@ -3,4 +3,14 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import reducer from './reducers'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+const store = createStore(reducer)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'))
